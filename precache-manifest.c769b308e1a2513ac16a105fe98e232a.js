486be634ce20b48410c552f0e257005e3a41ca68
mrfazry <mredykafazry@gmail.com>
2018-11-07 23:02:24 +0700

    Updates

---
+++ precache-manifest.c769b308e1a2513ac16a105fe98e232a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ee7fcb932540041e18ee4e034a14a89",
+    "url": "/weather-forecast/static/media/spinner.8ee7fcb9.gif"
+  },
+  {
+    "revision": "f2d38ab6dc1f45e8eef0",
+    "url": "/weather-forecast/static/js/runtime~main.f2d38ab6.js"
+  },
+  {
+    "revision": "599b04b4d282868607e7",
+    "url": "/weather-forecast/static/js/main.599b04b4.chunk.js"
+  },
+  {
+    "revision": "82ea8b01399a78bd05fc",
+    "url": "/weather-forecast/static/js/1.82ea8b01.chunk.js"
+  },
+  {
+    "revision": "599b04b4d282868607e7",
+    "url": "/weather-forecast/static/css/main.5486a50f.chunk.css"
+  },
+  {
+    "revision": "acb50c490a2f1561f98c9060e056eca2",
+    "url": "/weather-forecast/index.html"
+  }
+];
